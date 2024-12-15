69ab62f6915c957d04c560ff554af0cd07bfd1b2
Owen Healy <owen@owenehealy.com>
2024-12-15 17:07:30 -0500

    Umapper vanilla layout

---
+++ umapper/vanilla-layout.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "keys.h"
+
+#define VANILLA_LAYOUT_ROWS 6
+#define VANILLA_LAYOUT_COLS 14
+
+extern const __flash key_code vanilla_layout[VANILLA_LAYOUT_ROWS][VANILLA_LAYOUT_COLS];
+
